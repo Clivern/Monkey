3708e6fd09de7755c3b534f98dfae1c121001eca
Clivern <hello@clivern.com>
2018-03-04 13:15:21 +0100

    Add all possible cloudstack entities

---
+++ src/API/Caller.php
@@ -0,0 +1,14 @@
+<?php
+namespace Clivern\CloudStackMonkey\API;
+
+
+/**
+ * CloudStack API Caller Class
+ *
+ * @since 1.0.0
+ * @package Clivern\CloudStackMonkey\API
+ */
+class Caller
+{
+
+}
---
+++ src/API/Contract/RequestInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace Clivern\CloudStackMonkey\API\Contract;
+
+
+/**
+ * API Request Interface
+ *
+ * @since 1.0.0
+ * @package Clivern\CloudStackMonkey\API\Contract
+ */
+interface RequestInterface
+{
+
+}

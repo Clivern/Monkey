bbfe54785680d1745c0f27e7dc4b9d8ca49e0fa8
Clivern <hello@clivern.com>
2018-03-04 13:28:54 +0100

    test Travis

---
+++ tests/API/CallerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\API;
+
+use Clivern\CloudStackMonkey\API\Caller;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Caller Class Test
+ *
+ * @since 1.0.0
+ * @package Tests\API
+ */
+class CallerTest extends TestCase
+{
+    public function testClass()
+    {
+        $this->assertequals("Clivern\CloudStackMonkey\API\Caller", get_class(new Caller()));
+    }
+}

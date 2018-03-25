c5e163f9bd8cf23679896652da102235d62134d9
Clivern <hello@clivern.com>
2018-03-25 23:58:17 +0200

    Add jobs class

--- src/API/Job.php
+++ src/API/Job.php
@@ -1,6 +1,9 @@
 <?php
 namespace Clivern\Monkey\API;
 
+use Clivern\Monkey\API\Caller;
+use Clivern\Monkey\API\Factory;
+
 /**
  * CloudStack API Job Class
  *
@@ -9,4 +12,80 @@ namespace Clivern\Monkey\API;
  */
 class Job {
 
+	private $callers = [];
+	private $retryPerCaller = [];
+	private $trialsPerCaller = [];
+	private $status;
+
+
+	public function __construct($callers = [], $retryPerCaller = [])
+	{
+		$this->callers = $callers;
+		$this->retryPerCaller = array_merge($this->retryPerCaller, $retryPerCaller);
+	}
+
+	public function addCaller($ident, Caller $caller)
+	{
+		$this->callers[$ident] = $caller;
+	}
+
+	public function removeCaller($ident)
+	{
+		if ($this->callerExists($ident)) {
+			unset($this->callers[$ident]);
+		}
+
+		return !$this->callerExists($ident);
+	}
+
+	public function callerExists($ident)
+	{
+		return (isset($this->callers[$ident]));
+	}
+
+	public function addCallers($callers)
+	{
+		$this->callers = $callers;
+	}
+
+	public function getCallers()
+	{
+		return $this->callers;
+	}
+
+	public function getCaller($ident)
+	{
+		return $this->callerExists($ident) ? $this->callers[$ident] : null;
+	}
+
+	public function execute()
+	{
+		#~
+	}
+
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+    	$this->status = $status;
+    }
+
+	protected function updateCaller($ident)
+	{
+
+	}
+
+    public function dump($type)
+    {
+    	#~
+    }
+
+    public function reload($data, $type)
+    {
+    	#~
+    }
 }
